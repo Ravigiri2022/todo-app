9ff01a51a7889048288646fa8b746cd3d51ae80c
Jarvis <macbookair@MacBookAir.localdomain>
2025-10-17 10:06:14 +0600

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './pages/auth/auth-layout/auth-layout';
+import { Login } from './pages/auth/login/login';
+import { Signup } from './pages/auth/signup/signup';
+import { ForgotPassword } from './pages/auth/forgot-password/forgot-password';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () => AuthLayout,
+    children: [
+      {
+        path: 'login',
+        component: Login,
+      },
+      {
+        path: 'signup',
+        component: Signup,
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPassword,
+      },
+    ],
+  },
+];
---
+++ src/app/pages/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
+})
+export class Login {
+  onLogin() {}
+  onSignup() {}
+}
